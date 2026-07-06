eacfc718f6d17125e87b80e1321277f5adde9f87
Maxwilguentz-cmd <maxwilguentz@gmail.com>
2026-07-06 02:24:49 -0400

    Fix formatting of messagingSenderId in script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const firebaseConfig = {
     databaseURL: "https://bwdepot-61214-default-rtdb.firebaseio.com",
     projectId: "bwdepot-61214",
     storageBucket: "bwdepot-61214.firebasestorage.app",
-    messagingSenderId: "624010872324",
+    messagingSenderId: "624010872324", 
     appId: "1:624010872324:web:0b0565c3872b3caccd5751",  
     measurementId: "G-8VEN9QQF7B"
 };
