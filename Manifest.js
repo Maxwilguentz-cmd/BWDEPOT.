c39b37890afa14022603d90238dc43acb0715a41
Maxwilguentz-cmd <maxwilguentz@gmail.com>
2026-07-18 10:46:39 -0400

    Add Manifest.js for web app configuration

---
+++ Manifest.js
@@ -0,0 +1,9 @@
+{
+  "name": "BWdepot",
+  "short_name": "BWdepot",
+  "start_url": "index.html",
+  "display": "standalone",
+  "background_color": "#F1F5F9",
+  "theme_color": "#1E3A8A",
+  "icons": []
+}
